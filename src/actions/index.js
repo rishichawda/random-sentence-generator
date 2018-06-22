67af49254dfd7478ac0754c49b0f7f482f913aa1
Rishi Kumar <chawda.rishii@gmail.com>
2018-06-22 13:54:14 +0530

    fix for gh-pages

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const rtext = 'rtext';
 
 export function gettext() {
-    const url = 'http://www.randomtext.me/api/';
+    const url = 'https://www.randomtext.me/api/';
     const request = axios.get(url);
     return {
         type: rtext,
