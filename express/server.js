4850d0a36384e0b2ce2e423c95c4aeb25b497495
rishichawda <chawda.rishii@gmail.com>
2019-07-13 17:23:19 +0530

    build(server): move to netlify

--- express/server.js
+++ express/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const http = require('http')
+const serverless = require("serverless-http");
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
 const routes = require('./routes')
+
+const router = express.Router();
 
 const PORT = process.env.PORT || 8080
 
@@ -13,12 +16,15 @@ app.set('port', PORT)
 
 app.use(cors())
 app.use(bodyParser.json({ type: '*/*' }));
+app.use("/.netlify/functions/server", router);
 
 if ('development' == app.get('env')) {
   app.use(logger())
 }
 
 routes(app)
+
+module.exports.handler = serverless(app);
 
 // Start server on port.
 http.createServer(app).listen(PORT, function(){
